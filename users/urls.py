f9f64e17b5165a7ba82aea124cd3b61d43049c9e
shlomo809 <shlomo809@gmail.com>
2023-07-19 17:06:50 +0300

    chane re_path to path

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,9 @@
-from django.urls import re_path, path
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-   re_path(r"^user/create_new_user$", views.create_new_user),
+   path("user/create_new_user", views.create_new_user),
    path("user/<int:user_id>/login", views.update_login_status, {'is_logged_in': True}),
    path("user/<int:user_id>/logout", views.update_login_status, {'is_logged_in': False}),
 
