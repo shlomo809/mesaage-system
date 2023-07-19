27526c820a752c362b164d3746d1f87405daa0bb
shlomo809 <shlomo809@gmail.com>
2023-07-19 17:08:21 +0300

    test to see if herku get updated

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path("user/create_new_user", views.create_new_user),
+   path("user/create_new_userx", views.create_new_user),
    path("user/<int:user_id>/login", views.update_login_status, {'is_logged_in': True}),
    path("user/<int:user_id>/logout", views.update_login_status, {'is_logged_in': False}),
 
